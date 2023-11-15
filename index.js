0b33975809b45aad5c7ee4cd236844c51fa01aa5
AliAbdaly <106967881+AliAbdaly@users.noreply.github.com>
2023-11-15 17:22:01 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
